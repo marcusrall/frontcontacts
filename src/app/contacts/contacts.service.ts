1b5be9ef300353e492c26269ca1e444200cc8d07
marcusrall <marcus.almeira@webrall.com>
2017-02-11 19:02:55 -0300

    Layout dashboard contact

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -6,12 +6,27 @@ export class ContactsService {
   getContacts(){
     return [
       {id: 1, name: 'Marcus Almeida', company: 'Web Rall', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
-      {id: 1, name: 'Maria da Silva', company: 'Coca Cola', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
-      {id: 1, name: 'Jorge Alves', company: 'Apple', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
-      {id: 1, name: 'Paulo Rodolfo', company: 'Life Sistema', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
-      {id: 1, name: 'Caio de Souza', company: 'Samsung', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
-      {id: 1, name: 'Antonia Aquino', company: 'Web Rall', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' }
+      {id: 2, name: 'Maria da Silva', company: 'Coca Cola', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
+      {id: 3, name: 'Jorge Alves', company: 'Apple', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
+      {id: 4, name: 'Paulo Rodolfo', company: 'Life Sistema', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
+      {id: 5, name: 'Caio de Souza', company: 'Samsung', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' },
+      {id: 6, name: 'Antonia Aquino', company: 'Web Rall', email: 'contato@webrall.com', phone: '63 3312-1221', eddress: 'Rua 14 de Novembro' }
     ];
+  }
+
+  getContact(id: number){
+    let contacts = this.getContacts();
+
+    for(let i=0; i<contacts.length; i++)
+    {
+        let contact = contacts[i];
+        if(contact.id == id)
+        {
+          console.log(contact);
+          return contact;
+        }
+        return null;
+    }
   }
 
   constructor() { }
--- src/app/detail-contact/detail-contact.component.ts
+++ src/app/detail-contact/detail-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+
+import { ContactsService } from './../contacts/contacts.service';
 
 @Component({
   selector: 'app-detail-contact',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailContactComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  inscricao: Subscription;
+  contact: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private contactsService: ContactsService
+  ) {
+  console.log(route);
+  }
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+
+        // this.contact = this.contactsService.getContact(this.id);
+        // if(this.contact == null){
+        //   this.router.navigate(['/contacts'])
+        // }
+
+      }
+    );
+  }
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
